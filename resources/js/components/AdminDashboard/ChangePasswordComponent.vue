76060eb471a6dfd2821e296ce8be1765fe82a2fc
chrisvillaba <chrisvillaba@gmail.com>
2019-11-28 16:04:18 +0800

    Signed-off-by: chrisvillaba <chrisvillaba@gmail.com> changes password updated

--- resources/js/components/AdminDashboard/ChangePasswordComponent.vue
+++ resources/js/components/AdminDashboard/ChangePasswordComponent.vue
@@ -14,7 +14,7 @@ label.cmp {
     <div>
      <v-stepper v-model="e1">
     <v-stepper-header class="card-title">
-      <v-stepper-step :complete="e1 > 1" step="1">Confirm Password</v-stepper-step>
+      <v-stepper-step :complete="e1 > 1" step="1">Current Password</v-stepper-step>
       <v-divider></v-divider>
       <v-stepper-step :complete="e1 > 2" step="2">Set New Password</v-stepper-step>
       <v-divider></v-divider>
@@ -25,14 +25,19 @@ label.cmp {
 
       <v-stepper-content step="1">
          <v-card flat height="100px" class="mb-12">
-            <v-text-field v-model="login.current_password" prepend-icon="lock" name="Password" label="Enter Current Password" type="password"></v-text-field>
+           <v-alert type="error" v-show="alert.value == true">{{alert.error}}</v-alert>
+           <v-alert type="error" v-show="alert.value2 == true">{{alert.error2}}</v-alert>
+            <v-text-field v-model="login.current_password" prepend-icon="lock" name="Password" label="Enter Current Password" type="password" 
+            :rules="[rules.required, rules.min]">
+            </v-text-field>
           </v-card>
         <v-btn color="primary" @click="verifylogin"> Verify</v-btn>
       </v-stepper-content>
 
       <v-stepper-content step="2">
-         <v-card flat height="100px" class="mb-12">
-            <v-text-field prepend-icon="lock" v-model="editedPassword.newpass" name="Password" label="New Password" type="password"></v-text-field>
+         <v-card flat height="200px" class="mb-12">
+           <v-alert type="error" v-show="alert2.value == true">{{alert2.error}}</v-alert>
+            <v-text-field prepend-icon="lock" v-model="editedPassword.newpass" name="Password" label="New Password" type="password" :rules="[rules.required, rules.min]"></v-text-field>
             <v-text-field prepend-icon="lock" v-model="editedPassword.confirm_password" name="Password" label="Confirm New Password" type="password"></v-text-field>
           </v-card>
         <v-btn color="primary" @click="changepassword" >Change Password</v-btn>
@@ -40,7 +45,8 @@ label.cmp {
 
       <v-stepper-content step="3">
          <v-card flat height="100px" class="mb-12">
-           <label class="cmp">Your Password Has Been Updated</label>
+           <label class="cmp">Your Password Succesfully Updated!</label>
+           <p>Please click the "Reload" button to apply change password</p>
           </v-card>
         <v-btn color="primary" @click="completeChangePassword" >Reload</v-btn>
 
@@ -57,6 +63,21 @@ label.cmp {
 export default {
   data () {
       return {
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 6 || 'Min 6 characters',
+          // emailMatch: () => ('The email and password you entered don\'t match'),
+        },
+        alert: {
+          value: false,
+          value2: false,
+          error: '',
+          error2: '',
+          },
+        alert2: {
+          value:false,
+          error:'',
+        },
         login:{
           current_password:'',
         },
@@ -71,10 +92,21 @@ export default {
     verifylogin(){
       axios.post('/changepass/1',this.login)
       .then((res)=>{
-          if(res.data == 'Confirmed'){
-            this.e1 = 2
-          }
-      })
+        
+        if(res.data == 'Confirmed'){
+          this.e1 = 2
+            console.log(this.e1)
+              }else if(res.data == 'error'){
+                this.alert.value = true    
+                this.alert.value2 = false
+                this.alert.error = 'Please Enter Your Valid Password'    
+              }else if(res.data == 'empty' ){
+                console.log(res.data)
+                this.alert.value = false
+                this.alert.value2 = true
+                this.alert.error2 = 'Please Enter Your Current Password'    
+              }
+        })
     },
     changepassword(){
       if(this.editedPassword.newpass === this.editedPassword.confirm_password){
@@ -82,10 +114,14 @@ export default {
           .then((res)=>{
             if(res.data == 'Change Password Confirmed'){
               this.e1 = 3
+            }else if(res.data == 'empty'){
+              this.alert2.value = true              
+              this.alert2.error = 'Please Enter Your Current Password'              
             }
         })
       }else{
-        console.log('password do not match')
+        this.alert2.value = true;
+        this.alert2.error = 'password did not match';
       }
     },
     completeChangePassword(){
